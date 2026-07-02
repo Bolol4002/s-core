3d7d32bf4a9bdbe067eff2cc123eec7f384b83db
bolol4002 <sayoojsumesh1112@gmail.com>
2026-07-02 16:18:09 +0000

    2026-07-02 16:18:09

--- tb/signext.py
+++ tb/signext.py
@@ -2,7 +2,6 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, Timer
 import random
-import numpy as np
 
 @cocotb.test()
 async def random_write_read_test(dut):
@@ -16,22 +15,22 @@ async def random_write_read_test(dut):
     raw_data = random_junk | imm
     await Timer(1, units="ns")
     dut.raw_src.value = raw_data
-    dut.imm_source = source
+    dut.imm_source.value = source
     await Timer(1, units="ns") # let it propagate ...
     assert dut.immediate.value == "00000000000000000000000001111011"
     assert int(dut.immediate.value) == 123
 
     # TEST Negative IMM = -42 WITH SOURCE = 0
     imm = 0b111111010110 #-42
-    imm <<= 13 # leave "room" for ramdom junk
+    imm <<= 13 # leave "room" for random junk
     source = 0b00
-    # 25 bits sent to sign extend contains data before that will be ignred (rd, f3,..)
+    # 25 bits sent to sign extend contains data before that will be ignored (rd, f3,..)
     # masked to leave room for imm "test payload"
     random_junk = 0b000000000000_1010101010101 
     raw_data = random_junk | imm
     await Timer(1, units="ns")
     dut.raw_src.value = raw_data
-    dut.imm_source = source
+    dut.imm_source.value = source
     await Timer(1, units="ns") # let it propagate ...
     assert dut.immediate.value == "11111111111111111111111111010110"
     # Python interprets int as uint. we sub 1<<32 as int to get corresponding negative value
