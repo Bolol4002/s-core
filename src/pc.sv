3bdeba62dc8d39c074be5546c86551b30cb187a8
bolol4002 <sayoojsumesh1112@gmail.com>
2026-07-07 15:48:41 +0000

    change

--- src/pc.sv
+++ src/pc.sv
@@ -1,3 +1,15 @@
-module pc(input a, input b, output y);
-   assign y = ~(a & b);
+module pc (
+    input  logic        clk,
+    input  logic        rst,
+    input  logic [31:0] next_pc,
+    output logic [31:0] pc
+);
+
+    always_ff @(posedge clk) begin
+        if (rst)
+            pc <= 32'd0;
+        else
+            pc <= next_pc;
+    end
+
 endmodule
